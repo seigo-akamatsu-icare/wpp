01fec5bce7d39095ed17f53f1b294a463ecd5597
seigo-akamatsu-icare <seigo.akamatsu@icare.jpn.com>
2020-06-27 17:44:18 +0900

    Build css with sass

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,13 @@ module.exports = {
         path: path.resolve(__dirname,'./dist'),
         filename: './js/main.js',
     },
+    devServer: {
+        disableHostCheck: true
+    },
     module: {
         rules: [
             {
-                test: /\.css/,
+                test: /\.(css|sass|scss)/,
                 use: [
                     {
                         // loader: 'style-loader',
@@ -20,7 +23,10 @@ module.exports = {
                     },
                     {
                         loader: 'css-loader',
-                    }
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
                 ]
             },
             {
@@ -63,6 +69,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/templates/access.pug',
             filename: 'access.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/templates/members/taro.pug',
+            filename: 'members/taro.html',
         }),
         new CleanWebpackPlugin(),
     ]
